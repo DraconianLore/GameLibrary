b2b9e7718f36e56ed92973466740d81d365358b5
Steven Wing <steven.wing82@gmail.com>
2022-09-26 13:56:43 -0700

    tidy up files after removing config

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module Gamelibrary
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.launchers = config_for(:launchers)
   end
 end
